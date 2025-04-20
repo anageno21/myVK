c54411fe2c4f1ec622af5895237c51d29be240a1
anageno21 <p.makedon@anageno.com>
2025-04-20 09:21:17 +0300

    Added ProductPage placeholder to fix build error

---
+++ src/pages/ProductPage.js
@@ -0,0 +1,13 @@
+// src/pages/ProductPage.js
+import React from 'react';
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h2>Product Page Placeholder</h2>
+      <p>This is a placeholder for the Product page.</p>
+    </div>
+  );
+};
+
+export default ProductPage;
