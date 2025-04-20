a7a6c15bc61bfea8c59513556a0355fe43c7f1b5
anageno21 <p.makedon@anageno.com>
2025-04-20 07:32:55 +0300

    Removed unused CSS imports from index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './styles/animate.min.css';
-import './styles/swiper-bundle.min.css';
-import './styles/styles.css';
-import './styles/icomoon/style.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Περιμένουμε το DOM να φορτώσει
-window.onload = () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-};
+// Αφαίρεση των εισαγωγών που προκαλούν σφάλματα
+// import './styles/animate.min.css';
+// import './styles/swiper-bundle.min.css';
+// import './styles/styles.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
